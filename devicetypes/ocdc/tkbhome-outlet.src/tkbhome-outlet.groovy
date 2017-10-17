ca2b78e75d026f0ecc4acba8e95dbea57188e3e1
Owen Carr <owencarruk@gmail.com>
2017-10-17 14:06:40 +0100

    Fixed comments

--- devicetypes/ocdc/tkbhome-outlet.src/tkbhome-outlet.groovy
+++ devicetypes/ocdc/tkbhome-outlet.src/tkbhome-outlet.groovy
@@ -1,18 +1,23 @@
 /**
- *
  *  TKBHOME Z-Wave Plug - TZ68E
  *  Copyright 2016 Soon Chye
+ *
+ *  Notes:
+ *  By default the LED is set to 'Night-Light' mode, which means the LED is ON when the device is OFF. 
+ *  To change this so the LED is OFF when the device is OFF, set parameter 1 to 1.
+ *
+ *  Changelog:
+ *  1.1 - Tweaks made by ocdc
+ *  1.0 - First release
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- *
- *  By default the LED is set to 'Night-Light' mode, which means the LED is ON when the device is OFF. 
- *  To change this so the LED is OFF when the device is OFF, set parameter 1 to 1.
- *
- *  Version 1.0 - First release
- *  Version 1.1 - Tweaks made by ocdc
- * 
  */
 
 metadata {
